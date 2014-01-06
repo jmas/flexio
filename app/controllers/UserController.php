8179762935bd7e369ec12c13f786c55d49ac3a57
NiceBro <nicebro@mail.ua>
2014-01-07 01:20:07 +0200

    Revert "UserController bugfix"
    This reverts commit 680fb7c38f48a2e1f4902e21f2ae9c11e11c6216.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -30,6 +30,7 @@ class UserController extends Controller
 	public function indexAction()
 	{	
 		$models = App::instance()->models->findAll('User');
+		var_dump($models[0]->getPermissions());
 		echo $this->render('index', 
 			array(
 				'models' => $models
