78f85824e9b6b9443f643d2ec4bf53600032a1df
Alexander Maslakov <alexandr@ego-cms.com>
2014-01-06 21:40:22 +0200

    Setting controller updated.

---
+++ app/controllers/SettingController.php
@@ -0,0 +1,44 @@
+<?php
+
+class SettingController extends Controller
+{
+	public function beforeExec($actionName, array $params=array())
+	{
+		parent::beforeExec($actionName, $params);
+
+		if (! App::instance()->auth->isLoggedIn()) {
+			App::instance()->redirect(array(
+				'controller'=>'auth',
+				'action'=>'index',
+			));
+		}
+
+		$this->setLayoutValue('isNavEnabled', true);
+
+		return true;
+	}
+
+	/**
+	 *
+	 */
+	public function indexAction()
+	{
+		echo $this->render('index');
+	}
+
+	/**
+	 *
+	 */
+	public function pluginAction()
+	{
+		echo $this->render('plugin');
+	}
+
+	/**
+	 *
+	 */
+	public function updateAction()
+	{
+		echo $this->render('update');
+	}
+}
