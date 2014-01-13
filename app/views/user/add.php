8d1c9abbbc4f9baa25d00b7f88415a7da2b51a1c
NiceBro <nicebro@mail.ua>
2014-01-13 19:10:13 +0200

    Updating User views issue #1

--- app/views/user/add.php
+++ app/views/user/add.php
@@ -1,1 +1,44 @@
-User add <?php var_dump($this->model); ?>
+<div class="container">
+    
+  <div class="page-header">
+    <h2>Add user</h2>
+  </div>
+  
+  <form class="form-horizontal" role="form" method="post">
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Username</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Name</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Password</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Password confirm</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-4">
+        <button type="submit" class="btn btn-default">Add</button>
+      </div>
+    </div>
+  </form>
+</div>
--- app/views/user/edit.php
+++ app/views/user/edit.php
@@ -1,6 +1,9 @@
-User profile <?php var_dump($this->model); ?>
 <div class="container">
-  <form class="form-horizontal" role="form">
+
+  <div class="page-header">
+    <h2>Edit user: <?php echo $this->model->username; ?></h2>
+  </div>
+  <form class="form-horizontal" role="form" method="post">
     <div class="form-group">
       <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
       <div class="col-sm-4">
@@ -10,16 +13,19 @@ User profile <?php var_dump($this->model); ?>
     <div class="form-group">
       <label for="name" class="col-sm-2 control-label">Name</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name" value="<?php echo $this->model->name; ?>">
+        <input type="text" class="form-control" id="name" value="<?php echo $this->model->name; ?>">
       </div>
     </div>
     <div class="form-group">
-      <div class="col-sm-offset-2 col-sm-4">
-        <div class="checkbox">
-          <label>
-            <input type="checkbox"> Remember me
-          </label>
-        </div>
+      <label for="name" class="col-sm-2 control-label">Permissions</label>
+      <div class="col-sm-4">
+        <input type="text" class="form-control" id="name" value="<?php echo $this->model->permissions; ?>">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-4">
+        <input type="text" class="form-control" id="name" value="<?php echo $this->model->email; ?>">
       </div>
     </div>
     <div class="form-group">
@@ -27,5 +33,6 @@ User profile <?php var_dump($this->model); ?>
         <button type="submit" class="btn btn-default">Save</button>
       </div>
     </div>
+    
   </form>
 </div>
