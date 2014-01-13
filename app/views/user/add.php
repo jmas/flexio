f73e06e8c162b4cd446fe45c0bc76eb59756be1e
NiceBro <nicebro@mail.ua>
2014-01-14 00:54:57 +0200

    improvements in add, edit, delete methods issue #1

--- app/views/user/add.php
+++ app/views/user/add.php
@@ -4,35 +4,35 @@
     <h2>Add user</h2>
   </div>
   
-  <form class="form-horizontal" role="form" method="post">
+  <form class="form-horizontal" role="form" method="post" autocomplete="off">
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Username</label>
+      <label for="data[name]" class="col-sm-2 control-label">Name</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name">
+        <input type="text" class="form-control" name="data[name]">
       </div>
     </div>
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Name</label>
+      <label for="data[username]" class="col-sm-2 control-label">Username</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name">
+        <input type="text" class="form-control" name="data[username]">
       </div>
     </div>
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Email</label>
+      <label for="data[email]" class="col-sm-2 control-label">E-mail</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name">
+        <input type="email" class="form-control" name="data[email]" >
       </div>
     </div>
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Password</label>
+      <label for="data[name]" class="col-sm-2 control-label">Permission</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name">
+        <input type="text" class="form-control" name="data[permissions]" value="user">
       </div>
     </div>
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Password confirm</label>
+      <label for="data[password]" class="col-sm-2 control-label">Password</label>
       <div class="col-sm-4">
-        <input type="email" class="form-control" id="name">
+        <input type="password" class="form-control" name="data[password]">
       </div>
     </div>
     <div class="form-group">
