1b5f2e82f519faea7b2062c7f1af0e83154936ca
Alexander Maslakov <alexandr@ego-cms.com>
2014-01-30 23:50:33 +0200

    Added join functionlity to ModelManager.

--- app/views/layout/index.php
+++ app/views/layout/index.php
@@ -14,7 +14,7 @@
             <div class="col-md-2">Created: <?php echo $model->create_date; ?></div>
             <div class="col-md-2">Last update: <?php echo $model->update_date; ?></div>
             <div class="col-md-2">Created by: <a href="<?php echo $this->app->createUrl(array('user', 'edit', 'id'=>$model->create_user_id)); ?>"><?php echo $model->createdBy->name; ?></a></div>
-            <div class="col-md-2">Updated by: <a href="<?php echo $this->app->createUrl(array('user', 'edit', 'id'=>$model->create_user_id)); ?>"><?php echo $model->updatedBy->name; ?></a></div>
+            <div class="col-md-2">Updated by: <a href="<?php echo $this->app->createUrl(array('user', 'edit', 'id'=>$model->update_user_id)); ?>"><?php echo $model->updatedBy->name; ?></a></div>
             <div class="col-md-2 text-center"><a href="<?php echo $this->app->createUrl(array('layout', 'delete', 'id'=>$model->id)); ?>" type="button" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</a></div>
         </div>
     </div>
