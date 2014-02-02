d8f58f98858df0e375a88151ae004e6d21752a5e
Alexander Maslakov <alexandr@ego-cms.com>
2014-02-02 23:18:43 +0200

    Asset manager.

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
+<?php
+
+$this->app->assets->addCss(array(
+    'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css',
+    $this->app->getAssetUrl('css/app.css'),
+));
+
+$this->app->assets->addJs(array(
+    'http://code.jquery.com/jquery-1.10.2.min.js',
+    'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js',
+    'http://cdnjs.cloudflare.com/ajax/libs/parsley.js/1.2.2/parsley.min.js',
+));
+
+$this->app->assets->addJs($this->app->getAssetUrl('js/app.js'), AssetManager::POS_BODY_BOTTOM);
+
+?><!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
