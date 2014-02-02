7678794e1ee8eff5354f35101c8e2a95df8c0170
Alexander Maslakov <alexandr@ego-cms.com>
2014-02-03 00:54:11 +0200

    Updated plugins.

--- app/views/setting/plugin.php
+++ app/views/setting/plugin.php
@@ -10,21 +10,14 @@
 <div class="list-group">
     <div class="list-group-item list-group-item-info">
         <div class="row">
-            <div class="col-md-4">Name</div>
-            <div class="col-md-2">Author</div>
-            <div class="col-md-2">Version</div>
-            <div class="col-md-2">Last update</div>
-            <div class="col-md-2"></div>
+            <div class="col-md-10">Name</div>
         </div>
     </div>
     <?php foreach ($this->plugins as $plugin): ?>
     <div class="list-group-item">
         <div class="row">
-            <div class="col-md-4"><?php echo $plugin->getName(); ?></div>
-            <div class="col-md-2"></div>
-            <div class="col-md-2"><?php echo $plugin->getVersion(); ?></div>
-            <div class="col-md-2"></div>
-            <div class="col-md-2"><a href="<?php echo $this->app->createUrl(array('setting', $this->app->plugins->isInstalled($plugin->getName()) ?  'uninstall' : 'install', 'pluginName'=>$plugin->getName())); ?>" type="button" class="btn <?php echo $this->app->plugins->isInstalled($plugin->getName()) ? 'btn-danger': 'btn-success'; ?> btn-xs"><?php echo $this->app->plugins->isInstalled($plugin->getName()) ?  'Uninstall' : 'Install'; ?></a></div>
+            <div class="col-md-11"><?php echo $plugin->getName(); ?></div>
+            <div class="col-md-1"><a href="<?php echo $this->app->createUrl(array('setting', $this->app->plugins->isInstalled($plugin->getName()) ?  'uninstall' : 'install', 'pluginName'=>$plugin->getName())); ?>" type="button" class="btn <?php echo $this->app->plugins->isInstalled($plugin->getName()) ? 'btn-danger': 'btn-success'; ?> btn-xs"><?php echo $this->app->plugins->isInstalled($plugin->getName()) ?  'Uninstall' : 'Install'; ?></a></div>
         </div>
     </div>
     <?php endforeach; ?>
